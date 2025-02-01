32bc755ce593c32e56f30816573ac7e1d46bd152
Ferrer, Alastair <162070969+AlastairFerrer@users.noreply.github.com>
2025-02-01 19:09:45 +0800

    --homepage.php (css,images)

---
+++ homepage.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href='CSS\mainPage.css'></link>
+
+    <title>E-Commerce</title>
+</head>
+    <body>
+        <header class='main-page-header'>
+            <nav id='nav-left'>
+                <h1>E-Commerce</h1> 
+            </nav>
+            <nav id='nav-right'>
+                <a href="#">My Account</a>
+                <a href="#">Wish List</a>
+                <a href="#">Shopping</a>
+                <a href="#">Checkout</a>
+            </nav>
+        </header>
+        <main>
+            <div class='container'>
+                <div class='main-page-title'>
+                    <h1>EStore.</h1>
+                </div>
+
+                <div class='main-page-menu'>
+                    <a href="#">Home</a>
+                    <a href="#">Category</a>
+                    <p id="banner">hot</p>
+                    <a href="#">Latest</a>
+                    <a href="#">Blog</a>
+                    <a href="#">Pages</a>
+                    <a href="#">Contact</a>
+                </div>
+
+                <div class='main-page-search'>
+                    <input id="search" type="text" placeholder="Search Products">
+                    <button>Search</button>
+                    <button>Favorites</button>
+                    <button>Cart</button>
+                    <button>Sign In</button>
+                </div>
+            </div>
+
+            <div class='main-page-content'>
+                <div class='main-page-content-left'>
+                    <img src="images\person_1.png" alt="no image">
+                </div>
+
+                <div>
+                </div>
+
+            </div>
+    </body>
+</html>
